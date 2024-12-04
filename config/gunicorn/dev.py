bf89eee5a84df22b65e8653baf9bbbc59569bf68
sattorov <sattorov.ahu@gmail.com>
2024-12-04 20:39:59 +0500

    initial

--- config/gunicorn/dev.py
+++ config/gunicorn/dev.py
@@ -3,7 +3,7 @@ project_name = 'fruit-shop-backend'
 wsgi_app = "config.wsgi:application"
 loglevel = 'debug'
 workers = 1
-bind = '0.0.0.0:8008'
+bind = '0.0.0.0:8080'
 reload = False
 accesslog = errorlog = f'/var/log/gunicorn/{project_name}-dev.log'
 capture_output = True
